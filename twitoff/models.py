7ee72802ec3c905f9d664b2f04f67ae482d87987
Carlos Gutierrez <chgutier@gmail.com>
2019-03-05 11:22:36 -0600

    Things working with Anaconda prompt

--- twitoff/models.py
+++ twitoff/models.py
@@ -3,13 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
-# A model is just a class (this is a User class)
-# and inherits/it's a subclass of the DB.Model class
+
 class User(DB.Model):
     """Twitter users that we pull and analyze Tweets for."""
-    id = DB.Column(DB.Integer, primary_key =True)
+    id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
 
-class Tweet(DB.Moder):
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+class Tweet(DB.Model):
+    """Tweets."""
     id = DB.Column(DB.Integer, primary_key=True)
     text = DB.Column(DB.Unicode(280))
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
