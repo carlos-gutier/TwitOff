a0c3b848010f2f2d667edc69dbf9275f487717d3
Carlos Gutierrez <chgutier@gmail.com>
2019-03-05 06:06:17 -0600

    Adding new hello.py and twitoff folder with init and app.py files -- its been hard to make files run - had to add __if__name = __main__ to hello.py

---
+++ twitoff/app.py
@@ -0,0 +1,11 @@
+"""Main application and routing logic for TwitOff."""
+from flask import Flask
+
+def create_app():
+    """Create and configure and instance of the Flask application"""
+    app = Flask(__name__)
+
+    @app.route('/')
+    def root():
+        return 'Welcome to TwitOff!!'
+    return app
